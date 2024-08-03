7cd73459c3a7d2f7b797987573bac567fa6fad30
Karl Grossner <karl.geog@gmail.com>
2024-08-03 18:19:12 +0100

    updated ontology and context

--- src/validate_rdf.py
+++ src/validate_rdf.py
@@ -2,10 +2,15 @@ import os
 
 from rdflib import Graph
 
+filenames = ["/lpo/lpo_draft_jul20240729b.ttl",
+             "/lpo/lpo_context.jsonld",
+             "/data/vob10.jsonld",]
+formats = ["ttl", "json-ld"]
+
 cwd= os.getcwd()
 g = Graph()
 try:
-    g.parse(cwd+"/lpo/lpo_draft_jul20240729b.ttl", format="ttl")
+    g.parse(cwd+filenames[0], format=formats[0])
     print("The TTL file is valid.")
 except Exception as e:
     print(f"An error occurred: {e}")
